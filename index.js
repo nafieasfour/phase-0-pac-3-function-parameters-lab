404840e8321b05d6a8335585d96372f86ec46b3f
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-04-08 21:24:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name) {
+    return "Hi, my name is " +  name +".";
+}
+
+function introductionWithLanguage(n, language) {
+    return "Hi, my name is "+ n + " and I am learning to program in " + language+".";
+}
+
+function introductionWithLanguageOptional(n, language = "JavaScript") {
+    return "Hi, my name is "+ n + " and I am learning to program in " + language+".";
+}
+
+
